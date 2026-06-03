9bd1775bc8ee118e1e09628d7a14b3f7bf30c372
aj2nd <anandhujayaprakash@gmail.com>
2026-06-03 05:15:55 +0400

    Remove unnecessary nesting in translations file

--- client/src/contexts/translations/kn.ts
+++ client/src/contexts/translations/kn.ts
@@ -1,5 +1,4 @@
 export default {
-   kn: {
     home: "ಮುಖಪುಟ",
     settings: "ಸೆಟ್ಟಿಂಗ್‌ಗಳು",
     welcome_message: "RentFLO ಗೆ ಸ್ವಾಗತ",
@@ -389,5 +388,4 @@ export default {
     didit_opening: "Didit ತೆರೆಯಲಾಗುತ್ತಿದೆ…",
     didit_redirect_note: "ನಿಮ್ಮನ್ನು Didit ಗೆ ಕಳುಹಿಸಲಾಗುತ್ತದೆ ಮತ್ತು ಮುಗಿದ ಮೇಲೆ RentFLO ಗೆ ಮರಳಿ ತರಲಾಗುತ್ತದೆ.",
 
-},
 };
